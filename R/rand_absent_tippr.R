b6c9d70c12a76b2cb1a9b74969ef691cb5748948
April Wright <wright.aprilm@gmail.com>
2018-07-10 18:20:26 -0500

    pre-merge

--- R/rand_absent_tippr.R
+++ R/rand_absent_tippr.R
@@ -1,25 +1,26 @@
-#' Add tips not on existing tree to the tree at random, if they do not have congeners on the tree
+#' Add tips not on existing tree to the tree at random, if they do not have congeners on the tree.
 #'
-#' @param absent_list Vector of taxa in the total dataset that are not on the tree
 #' @param tree Starting tree; object of type phylo
+#' @param absent_list Vector of taxa in the total dataset that are not on the tree
 #' @import phytools
-#' @return tree. Phylo object containing the starting tree,
+#' @return tree Phylo object containing the starting tree,
 #'          and all tips that were added.
 #' @export
 #'
 
-rand_absent_tippR <- function(absent_list, tree){
-  if(!inherits(tree,"phylo")){
+rand_absent_tippr <- function(tree, absent_list){
+#Check that tree is of object phylo
+  if (!inherits(tree, "phylo")){
     stop("tree must be of class 'phylo'")
   }
   lost_df <- get_lost(absent_list, tree)
 
   for (row in 1:nrow(lost_df)) {
     full <- as.character(lost_df[[row, "B"]])
-    sprintf('Adding tips at random: %s', full)
-    nodel <- tree$edge[,2]
+    sprintf("Adding tips at random: %s", full)
+    nodel <- tree$edge[, 2]
     num <- sample(nodel, 1)
-    tree <- bind.tip(tree, full, where=num)
+    tree <- bind.tip(tree, full, where = num)
   }
   return(tree)
 }
