eee7086ba95fb1f079ea10955a1c3606e0ed9ba5
April Wright <wright.aprilm@gmail.com>
2018-07-23 15:56:07 -0500

    updated data docs

---
+++ R/ants.R
@@ -0,0 +1,13 @@
+#' Dataset of Morphological character for extant and extinct ants
+#'
+#' A dataset containing a phlogeny and taxonomy info for Formicidae
+#'
+#' @docType data
+#'
+#' @usage data(ants)
+#'
+#' @format tax_list a two-column dataset of taxon names and ages
+#' @format mrca_df a two-column dataframe of taxa to be placed, and a set of taxa that indicate the MRCA
+#' which they will subtend
+#' @format tree A phylogenetic tree to which tips will be added
+"ants"
---
+++ R/bears.R
@@ -0,0 +1,14 @@
+#' Dataset of Morphological character for extant and extinct bears
+#'
+#' A dataset containing 18 taxa and 62 characters for bears
+#'
+#' @docType data
+#'
+#' @usage data(bears)
+#'
+#' @format tax_list a two-column dataset of taxon names and ages
+#' @format mrca_df a two-column dataframe of taxa to be placed, and a set of taxa that indicate the MRCA
+#' which they will subtend
+#' @format tree A phylogenetic tree to which tips will be added
+#' @source \url{https://github.com/revbayes/revbayes_tutorial/tree/master/RB_TotalEvidenceDating_FBD_Tutorial/data}
+"bears"
