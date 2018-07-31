a507aab4e3bd9d9ed653e7ec12bb35fd47d49335
April Wright <wright.aprilm@gmail.com>
2018-07-31 08:58:05 -0500

    adding runtime + unit tests for higher-order taxa

---
+++ R/taxon_testr.R
@@ -0,0 +1,15 @@
+#' Utility function to check taxon_names in parsed data file
+#'
+#' @param dataf Taxon list
+#' @return None
+#'
+taxon_testr <- function(tax_list) {
+    if (all(grepl("apple", tax_list$taxon))) {
+      message("Taxon list names formatted correctly")
+    }
+    else {
+      warning("Taxon list tips must be formmatted in genus_species format. If this is a higher order taxon with no species name, please format as taxon_sp.")
+      stop
+    }
+    return(0)
+  }
